1eb260341e993c7c21f6956e552eab5d3a50c034
mgGummy <74232718+mgGummy@users.noreply.github.com>
2021-03-04 21:51:28 +0800

    Update yuanzu.py

--- PYHcsc/yuanzu.py
+++ PYHcsc/yuanzu.py
@@ -17,13 +17,18 @@
 # print(t1)
 # print(type(t1))
 
-t=(10,[20,30],9)
-print(t)
-print(type(t))
-print(t[0],type(t[0]),id(t[0]))
-print(t[1],type(t[1]),id(t[1]))
-print(t[2],type(t[2]),id(t[2]))
-'''尝试修改'''
-# t[1]=100  不可以修改，元组不允许修改元素
-t[1].append(100)
-print(t[1],type(t[1]),id(t[1]))
+# t=(10,[20,30],9)
+# print(t)
+# print(type(t))
+# print(t[0],type(t[0]),id(t[0]))
+# print(t[1],type(t[1]),id(t[1]))
+# print(t[2],type(t[2]),id(t[2]))
+# '''尝试修改'''
+# # t[1]=100  不可以修改，元组不允许修改元素
+# t[1].append(100)
+# print(t[1],type(t[1]),id(t[1]))
+
+'''元组的遍历'''
+t=('py','world',98)
+for item in t:
+    print(item)
