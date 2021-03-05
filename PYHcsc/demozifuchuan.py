cd232ba8f0fb95588a0bbbd75e1e461d525c0f85
mgGummy <74232718+mgGummy@users.noreply.github.com>
2021-03-05 13:40:04 +0800

    Create demozifuchuan.py

---
+++ PYHcsc/demozifuchuan.py
@@ -0,0 +1,12 @@
+#-*- codeing = utf-8 -*-
+#@Time : 2021/3/5 13:12
+#@Author : cscnb
+#@File : demozifuchuan.py
+#@Software : PyCharm
+#字符串的定义
+a='p'
+b="p"
+c='''p'''
+print(a,id(a))
+print(b,id(b))
+print(c,id(c))
