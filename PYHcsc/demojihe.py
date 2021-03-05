5f7c8de4d836b0860550bfcac845d1923ca2fdfa
mgGummy <74232718+mgGummy@users.noreply.github.com>
2021-03-05 12:02:27 +0800

    Update demojihe.py

--- PYHcsc/demojihe.py
+++ PYHcsc/demojihe.py
@@ -38,16 +38,20 @@
 # s2={30,40,20,10}
 # print(s==s2)
 # print(s!=s2)
-'''一个集合是否是另一个集合的子集'''
-s1={10,20,30,40,50,60}
-s2={10,20,30,40}
-s3={10,20,90}
-print(s2.issubset(s1))
-print(s3.issubset(s1))
-'''一个集合是否是另一个集合的超集'''
-print(s1.issuperset(s2))
-print(s1.issuperset(s3))
-'''两个集合是否有交集'''
-print(s2.isdisjoint(s3))
-s4={100,200,300}
-print(s2.isdisjoint(s4))
+# '''一个集合是否是另一个集合的子集'''
+# s1={10,20,30,40,50,60}
+# s2={10,20,30,40}
+# s3={10,20,90}
+# print(s2.issubset(s1))
+# print(s3.issubset(s1))
+# '''一个集合是否是另一个集合的超集'''
+# print(s1.issuperset(s2))
+# print(s1.issuperset(s3))
+# '''两个集合是否有交集'''
+# print(s2.isdisjoint(s3))
+# s4={100,200,300}
+# print(s2.isdisjoint(s4))
+
+#集合生成式
+s={ i * i for i in range(6)}
+print(s)
