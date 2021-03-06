5bd004774e75ff20e497c57f020a6603dbe79d18
mgGummy <74232718+mgGummy@users.noreply.github.com>
2021-03-06 16:19:33 +0800

    Update demozifuchuan.py

--- PYHcsc/demozifuchuan.py
+++ PYHcsc/demozifuchuan.py
@@ -12,8 +12,19 @@
 # print(c,id(c))
 
 #字符串的查询
-s='hello,hello'
-print(s.index('lo'))
-print(s.find('lo'))
-print(s.rindex('lo'))
-print(s.rfind('lo'))
+# s='hello,hello'
+# print(s.index('lo'))
+# print(s.find('lo'))
+# print(s.rindex('lo'))
+# print(s.rfind('lo'))
+
+#字符串大小写转换的方法
+s='hello,xxx'
+a=s.upper()
+print(a)
+print(s.lower())#小写,转换之后会产生一个新的字符串对象，虽然内容一样
+print(id(s))
+print(id(s.lower()))
+print(s.swapcase())
+print(s.capitalize())
+print(s.title())
