aa70612edc90d8c6f0284b03c6f611d66cc1aa56
mgGummy <74232718+mgGummy@users.noreply.github.com>
2021-03-06 21:13:47 +0800

    Update demozifuchuan.py

--- PYHcsc/demozifuchuan.py
+++ PYHcsc/demozifuchuan.py
@@ -19,12 +19,67 @@
 # print(s.rfind('lo'))
 
 #字符串大小写转换的方法
-s='hello,xxx'
-a=s.upper()
-print(a)
-print(s.lower())#小写,转换之后会产生一个新的字符串对象，虽然内容一样
-print(id(s))
-print(id(s.lower()))
-print(s.swapcase())
-print(s.capitalize())
-print(s.title())
+# s='hello,xxx'
+# a=s.upper()
+# print(a)
+# print(s.lower())#小写,转换之后会产生一个新的字符串对象，虽然内容一样
+# print(id(s))
+# print(id(s.lower()))
+# print(s.swapcase())
+# print(s.capitalize())
+# print(s.title())
+
+# #字符串内容对齐
+# s='hello,wdnmd'
+# #居中对齐
+# print(s.center(20,'*'))
+# #左对齐
+# print(s.ljust(20,'*'))
+# #右对齐
+# print(s.rjust(20,'*'))
+# #右对齐，使用0进行填充
+# print(s.zfill(20))
+# print('-4545'.zfill(8))
+#
+# #字符串的劈分
+# s='hello word wdnmd'
+# lst=s.split()
+# print(lst)
+# s1='hello|word|wdnmd'
+# print(s1.split())
+# print(s1.split(sep='|'))
+# print(s1.split(sep='|',maxsplit=1))
+# #rsplit 从右边开始劈分
+# print(s.rsplit())
+# print(s1.rsplit(sep='|',maxsplit=1))
+
+#字符串判断的相关方法
+# s='hello,python'
+# print('1.',s.isidentifier())
+# print('2.','hello'.isidentifier())
+#
+# print('\t'.isspace())
+#
+# print('abc'.isalpha())
+# print('张三'.isalpha())
+#
+# print('123'.isdecimal())
+#
+# print('123'.isnumeric())
+# print('123四'.isnumeric())
+#
+# print('abcd'.isalnum())
+# print('abcd张三123'.isalnum())
+
+#字符串的替换和合并
+s='hello wdnmd'
+print(s.replace('hello','你好'))
+s1='hello,python,python,python'
+print(s1.replace('python','java',2))
+lst=['hello','python','java']
+print('|'.join(lst))
+ls=('hello','wdnmd')
+print('|'.join(ls))
+print('*'.join('cscnb'))
+
+#字符串的比较操作
