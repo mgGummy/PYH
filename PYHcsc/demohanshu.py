2aa6615ec46994072c6d472baa2f2f282e89fea3
mgGummy <74232718+mgGummy@users.noreply.github.com>
2021-03-08 22:03:25 +0800

    Update demohanshu.py

--- PYHcsc/demohanshu.py
+++ PYHcsc/demohanshu.py
@@ -3,3 +3,21 @@
 #@Author : cscnb
 #@File : demohanshu.py
 #@Software : PyCharm
+def fun(a,b):
+    print('a',a)
+    print('b',b)
+    a=100
+    b.append(10)
+    print('a',a)
+    print('b',b)
+n1=11
+n2=[22,20]
+print(n1)
+print(n2)
+fun(n1,n2)
+print(n1)
+print(n2)
+'''在函数调用过程中，进行参数的传递
+    如果是不可变对象，在函数体的修改不会影响实参的值
+    如果是可变对象，在函数体的修改会影响实参的值
+'''
