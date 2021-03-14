cac974519c05c3facf152008f2e8672cd81b3a18
mgGummy <74232718+mgGummy@users.noreply.github.com>
2021-03-14 21:07:37 +0800

    Create demoneirongmokuai.py

---
+++ PYHcsc/demoneirongmokuai.py
@@ -0,0 +1,14 @@
+#-*- codeing = utf-8 -*-
+#@Time : 2021/3/14 20:57
+#@Author : cscnb
+#@File : demoneirongmokuai.py
+#@Software : PyCharm
+import sys
+import time
+import urllib
+print(sys.getsizeof(24))
+print(sys.getsizeof(48))
+print(sys.getsizeof(True))
+print(sys.getsizeof(False))
+print(time.time())
+print(time.localtime(time.time()))
