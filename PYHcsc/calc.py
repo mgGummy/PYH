2a3e29a1b6be81bc7c1a0eb0ef336994e63dea90
mgGummy <74232718+mgGummy@users.noreply.github.com>
2021-03-14 16:55:38 +0800

    Create calc.py

---
+++ PYHcsc/calc.py
@@ -0,0 +1,10 @@
+#-*- codeing = utf-8 -*-
+#@Time : 2021/3/14 15:05
+#@Author : cscnb
+#@File : calc.py
+#@Software : PyCharm
+def add(a,b):
+    return a+b
+
+if __name__=='__main__':
+    print(add(10,20))#只有当运行calc时，才会执行
