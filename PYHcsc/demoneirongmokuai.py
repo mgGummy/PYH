d32f3e4c0feb8260feb87855c23a956a55f4f3d3
mgGummy <74232718+mgGummy@users.noreply.github.com>
2021-03-14 21:42:11 +0800

    Update demoneirongmokuai.py

--- PYHcsc/demoneirongmokuai.py
+++ PYHcsc/demoneirongmokuai.py
@@ -5,10 +5,11 @@
 #@Software : PyCharm
 import sys
 import time
-import urllib
+import urllib.request
 print(sys.getsizeof(24))
 print(sys.getsizeof(48))
 print(sys.getsizeof(True))
 print(sys.getsizeof(False))
 print(time.time())
 print(time.localtime(time.time()))
+print(urllib.request.urlopen('http://www.baidu.com').read())
