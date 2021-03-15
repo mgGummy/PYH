e036faed7e1e9923d16d272eeb94adaccff772f3
mgGummy <74232718+mgGummy@users.noreply.github.com>
2021-03-15 22:06:56 +0800

    Create demowenjianduxie.py

---
+++ PYHcsc/demowenjianduxie.py
@@ -0,0 +1,16 @@
+#-*- codeing = utf-8 -*-
+#@Time : 2021/3/15 16:17
+#@Author : cscnb
+#@File : demowenjianduxie.py
+#@Software : PyCharm
+# file=open('a.txt','r')
+# print(file.readlines())
+# file.close()
+
+# file=open('b.txt','w')
+# file.write('hello csc')
+# file.close()
+
+file=open('b.txt','w')
+file.write('hello csc')
+file.close()
