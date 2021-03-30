54f3085a698eae570a1465d0a4c229a5ace22886
mgGummy <74232718+mgGummy@users.noreply.github.com>
2021-03-30 22:26:28 +0800

    Update text5.py

--- PYHcsc/pachong/text5.py
+++ PYHcsc/pachong/text5.py
@@ -12,10 +12,15 @@ if __name__ == '__main__':
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36 Edg/89.0.774.56'
     }
     url = 'https://www.qiushibaike.com/imgrank/page/%d/'
-    for yema in range(1,2):
+    for yema in range(1,3):
         new_url=format(url%yema)
         pa_text=requests.get(url=new_url,headers=headers).text
         ex='<div class="thumb">\n\n<a href=".*?" target="_blank">\n<img src="(.*?)" alt=".*?" class="illustration" width="100%" height="auto">\n</a>\n</div>'
         tupian = re.findall(ex,pa_text,re.S)
         print(tupian)
+        for src in tupian:
+            tupian_url = 'https' + src
+            print(tupian_url)
+
+
         
