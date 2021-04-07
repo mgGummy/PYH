e8784ac7cb8d5f2da574da1ef5362e1e2ad5b6c4
mgGummy <74232718+mgGummy@users.noreply.github.com>
2021-04-07 23:44:47 +0800

    Update text7.py

--- PYHcsc/pachong/text7.py
+++ PYHcsc/pachong/text7.py
@@ -3,3 +3,16 @@
 #@Author : cscnb
 #@File : text7.py
 #@Software : PyCharm
+
+#猪八戒网
+import requests
+from lxml import etree
+url = 'https://zhuhai.zbj.com/search/f/?type=new&kw=saas'
+zhubajieyuanma = requests.get(url)
+tree = etree.HTML(zhubajieyuanma.text)
+fangkuang = tree.xpath("/html/body/div[6]/div/div/div[2]/div[6]/div[1]/div")
+# print(fangkuang)
+for gebufen in fangkuang:
+    jiage = gebufen.xpath("./div/div[2]/div[2]/i/text()")
+    mingchen = 'saas'.join(gebufen.xpath("./div/div[2]/div[3]/a/text()"))
+    
